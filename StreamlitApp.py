4710c79965a5ba6c28567c1e3556118ae4f3e292
TimurTKO <151398944+TimurTKO@users.noreply.github.com>
2024-02-13 22:20:04 +0300

    Added some changes in Streamlit App

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -84,89 +84,6 @@ import streamlit as st
 
 
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
-import sklearn
-sklearn.set_config(transform_output="pandas")
-from sklearn.utils import compute_sample_weight
-#
-from sklearn.metrics import precision_recall_curve
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
-from sklearn.metrics import f1_score
-from sklearn.metrics import confusion_matrix
-from sklearn.linear_model import Ridge
-from sklearn import metrics
-from sklearn.metrics import accuracy_score
-from sklearn.base import BaseEstimator, TransformerMixin
-import seaborn as sns
-
-
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.tree import plot_tree
-
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-
-from sklearn.neighbors import KNeighborsClassifier
-
-from sklearn.preprocessing import RobustScaler
-
-from sklearn.cluster import KMeans
-
-from sklearn.ensemble import GradientBoostingClassifier
-
-from sklearn.svm import SVC
-
-import xgboost as xgb
-from xgboost import XGBRegressor, XGBClassifier
-
-from sklearn.pipeline import Pipeline
-
-from sklearn.compose import ColumnTransformer
-
-import optuna
-
-import mlflow
-
-import warnings
-
-warnings.simplefilter('ignore')
-
-import joblib
-
-from joblib import dump, load
-
-import requests
-
-client = mlflow.MlflowClient(tracking_uri="http://127.0.0.1:8084")
-
-mlflow.set_tracking_uri("http://127.0.0.1:8084")
-
-import requests
-
-from sklearn import impute
-
-from sklearn.impute import KNNImputer
-
-from sklearn.experimental import enable_iterative_imputer
-
-from sklearn.impute import IterativeImputer
-
-from imblearn.over_sampling import SMOTE
-
-from imblearn.pipeline import Pipeline
-
-from sklearn.preprocessing import LabelEncoder
-
-
 import streamlit as st
 import pandas as pd
 from joblib import load
@@ -177,25 +94,24 @@ model = load('bestmodel.joblib')
 # Create a Streamlit application
 st.title('Diabetes Prediction App')
 
-# Create a form to input the necessary features
-with st.form(key='my_form'):
-    st.write('Please input the following information:')
-    data = st.number_input(label='smoking_history', min_value=0)
-    HbA1c_level = st.number_input(label='HbA1c_level', min_value=0)
-    Gender = st.number_input(label='gender', min_value=0)
-    Hypertension = st.number_input(label='hypertension', min_value=0)
-    Blood_glucose_level = st.number_input(label='blood_glucose_level', min_value=0)
-    BMI = st.number_input(label='bmi', min_value=0.0)
-    Heart_disease = st.number_input(label='heart_disease', min_value=0)
-    DiabetesPedigreeFunction = st.number_input(label='DiabetesPedigreeFunction', min_value=0.0)
-    Age = st.number_input(label='age', min_value=0)
-    submit_button = st.form_submit_button(label='Predict')
+# load the dataset
+def predict_diabetes(data):
+    predictions = model.predict(data)
+    return predictions
 
-# Use the model to make a prediction based on the input features
-if submit_button:
-    input_data = pd.DataFrame([[Smoking_history, HbA1c_level, Gender, Hypertension, Blood_glucose_level, BMI, Heart_disease, DiabetesPedigreeFunction, Age]],
-                              columns=['smoking_history', 'HbA1c_level', 'gender', 'hypertension', 'blood_glucose_level', 'bmi', 'DiabetesPedigreeFunction', 'age', 'heart_disease'])
-    prediction = model.predict(input_data)
+uploaded_file = st.file_uploader("Upload your dataset", type=['csv'])
 
-    # Display the prediction on the Streamlit application
-    st.write('The likelihood of having diabetes is:', prediction[0])
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file)
+    st.write(data)
+    predictions = predict_diabetes(data)
+    st.write(predictions)
+
+
+
+
+
+
+
+
+#%%
